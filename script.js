406b56794832bb95f5a1bb1ca8c2a046843ceb30
Alisafa60 <alisafa6100@gmail.com>
2023-11-09 02:36:35 +0200

    New edit buttom + task prioirity and due date

--- script.js
+++ script.js
@@ -1,40 +1,80 @@
 //basic task managment
-const taskForm = document.getElementById("task-form")
-const taskInput = document.getElementById("task-input")
-const taskList = document.getElementById("task-list")
+const taskForm = document.getElementById("taskForm");
+const taskList = document.getElementById("task-list");
+const taskNameInput = document.getElementById("taskName");
+const dueDateInput = document.getElementById("dueDate");
+const priorityInput = document.getElementById("priority");
 
-let add_btn = document.getElementById("add-button")
-
-//create a new task item
 taskForm.addEventListener("submit", function(e) {
-    e.preventDefault();  
-    const taskName = taskInput.value.trim();
-    
-    if (taskName !== ""){
+    e.preventDefault();
+  
+    const taskName = taskNameInput.value.trim();
+    const dueDate = dueDateInput.value;
+    const priority = priorityInput.value;
+
+    if (taskName !== "") {
         const taskItem = document.createElement('li');
-        taskItem.innerHTML= `
-        <span>${taskName}</span>
+        taskItem.classList.add("task-item"); 
+        taskItem.innerHTML = `
+        <div class="task-name">${taskName}</div>
+        <div class="task-date">Due Date: ${dueDate}</div>
+        <div class="task-priority">Priority: ${priority}</div>
         <button class="edit-button">Edit</button>
         <button class="completed-button">Completed</button>
         <button class="delete-button">Delete</button>
         `;
         taskList.appendChild(taskItem);
-        taskInput.value = "";
-
+        taskNameInput.value = "";
+        dueDateInput.value = "";
     }
 });
 
-//task deletion and completion
-taskList.addEventListener("click", function(e){
+
+taskList.addEventListener("click", function (e) {
     const target = e.target;
-    if(target.classList.contains("delete-button")){
-        taskList.remove();
-    }
-    else if(target.classList.ceontain("complete-button")){
+    if (target.classList.contains("edit-button")) {
+        // Edit task
+        const taskItem = target.parentElement;
+        const taskText = taskItem.querySelector("span").innerText;
+        editTask(taskItem, taskText);
+    } else if (target.classList.contains("completed-button")) {
+        // Mark task as completed
         const task = target.parentElement;
         task.classList.add("completed");
+    } else if (target.classList.contains("delete-button")) {
+        // Delete task
+        const task = target.parentElement;
+        task.remove();
     }
-})
+});
+
+// Function to edit a task 
+function editTask(taskItem, taskText) {
+    const taskTextSpan = taskItem.querySelector("span");
+    taskTextSpan.style.display = "none";
+
+    const editInput = document.createElement("input");
+    editInput.type = "text";
+    editInput.value = taskText;
+    taskItem.appendChild(editInput);
+
+    const saveButton = document.createElement("button");
+    saveButton.innerText = "Save";
+    taskItem.appendChild(saveButton);
+
+    saveButton.addEventListener("click", function () {
+        const editedText = editInput.value;
+        if (editedText.trim() !== "") {
+            taskTextSpan.innerText = editedText;
+            taskTextSpan.style.display = "inline";
+            editInput.remove();
+            saveButton.remove();
+        }
+    });
+    
+}
+
+
 
 
 
