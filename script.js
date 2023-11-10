e98a5a8e27d478223bf580d5490b3860b3f7e525
Alisafa60 <alisafa6100@gmail.com>
2023-11-10 18:53:31 +0200

    styling the list with flex

--- script.js
+++ script.js
@@ -5,23 +5,36 @@ const taskNameInput = document.getElementById("taskName");
 const dueDateInput = document.getElementById("dueDate");
 const priorityInput = document.getElementById("priority");
 
+//creating a task list
 taskForm.addEventListener("submit", function(e) {
     e.preventDefault();
   
     const taskName = taskNameInput.value.trim();
-    const dueDate = dueDateInput.value;
     const priority = priorityInput.value;
+    const dueDate = dueDateInput.value;
+    let formattedDueDate;
+
+    if (dueDate.trim() === "") {
+        formattedDueDate = "Not specified";
+    } else {
+        formattedDueDate = dueDate;
+    }
+
 
     if (taskName !== "") {
         const taskItem = document.createElement('li');
         taskItem.classList.add("task-item"); 
         taskItem.innerHTML = `
-        <div class="task-name"><span>${taskName}<span></div>
-        <div class="task-date"><span>Due Date: ${dueDate}<span></div>
-        <div class="task-priority"><span>Priority: ${priority}<span></div>
-        <button class="edit-button">Edit</button>
-        <button class="completed-button">Completed</button>
-        <button class="delete-button">Delete</button>
+        <div class="task-item-container">
+            <span class="task-item-1 task-name">${taskName}</span>
+            <span class="task-item-1 task-date">${formattedDueDate}</span>
+            <span class="task-item-1 task-priority">${priority}</span>
+            <div class="button-container">    
+                <button class="task-item-1 edit-button"><i class="fas fa-edit" style="color: #b19cd8;"></i></button>
+                <button class="task-item-1 completed-button"><i class="fas fa-tasks" style="color: #b19cd9;"></i></button>
+                <button class="task-item-1 delete-button"><i class="fas fa-trash-alt" style="color: #b19cd8;"></i></button>
+            </div>
+        </div>
         `;
         taskList.appendChild(taskItem);
         taskNameInput.value = "";
@@ -34,21 +47,21 @@ taskList.addEventListener("click", function (e) {
     const target = e.target;
     if (target.classList.contains("edit-button")) {
         // Edit task
-        const taskItem = target.parentElement;
+        const taskItem = target.closest(".task-item");
         const taskText = taskItem.querySelector("span").innerText;
         editTask(taskItem, taskText);
     } else if (target.classList.contains("completed-button")) {
         // Mark task as completed
-        const task = target.parentElement;
-        task.classList.add("completed");
+        const taskItem = target.closest(".task-item")
+        taskItem.classList.add("completed");
     } else if (target.classList.contains("delete-button")) {
         // Delete task
-        const task = target.parentElement;
+        const task = target.closest(".task-item")
         task.remove();
     }
 });
 
-// Function to edit a task 
+// editing a task 
 function editTask(taskItem, taskText) {
     const taskTextSpan = taskItem.querySelector("span");
     taskTextSpan.style.display = "none";
@@ -73,3 +86,10 @@ function editTask(taskItem, taskText) {
     });
     
 }
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const backgroundImg = document.getElementById("backgroundImage");
+//     const imagePath = "./Assets/images/background.jpg";
+//     backgroundImg.style.backgroundImage = `url('${imagePath}')`;
+// });
