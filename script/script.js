113cb80674a4dac8c7c5e0d85c9da9ffa90ed533
Alisafa60 <alisafa6100@gmail.com>
2023-11-13 16:47:07 +0200

    adding more data in complete task list and fixing restore

--- script/script.js
+++ script/script.js
@@ -5,6 +5,7 @@ const taskNameInput = document.getElementById("taskName");
 const dueDateInput = document.getElementById("dueDate");
 const priorityInput = document.getElementById("priority");
 const completedTaskContainer = document.getElementById("completedTaskList");
+const toggleButton = document.getElementById("toggleCompletedTasks");
 
 //creating a task list
 taskForm.addEventListener("submit", function(e) {
@@ -52,32 +53,17 @@ taskList.addEventListener("click", function (e) {
     } else if (target.classList.contains("completed-button")) {
         // Mark task as completed
         const taskItem = target.closest(".task-item")
+        const taskName = taskItem.querySelector(".task-name").innerText;
+        const dueDate = taskItem.querySelector(".task-date").innerText;
+        const priority = taskItem.querySelector(".task-priority").innerText;
         taskItem.classList.add("completed");
-        moveCompletedTask(taskItem);
+        moveCompletedTask(taskItem, taskName, dueDate, priority);
     } else if (target.classList.contains("delete-button")) {
         // Delete task
         const task = target.closest(".task-item")
         task.remove();
     }
 });
-
-    //moving completed task to separate table
-function moveCompletedTask(taskItem) {
-    const taskText = taskItem.querySelector(".task-name").innerText;
-    const completedTaskItem = document.createElement('li');
-    completedTaskItem.classList.add("task-item", "completed-task-item");
-    completedTaskItem.innerHTML = `
-        <div class="task-item-container">
-            <span class="task-item task-name">${taskText}</span>
-            <div class="button-container">
-                <button class="restore-button"><i class="fas fa-undo" style="color: #b19cd8;"></i></button>
-            </div>
-        </div>
-    `;
-    completedTaskContainer.appendChild(completedTaskItem);
-    taskItem.remove();
-}
-
 
 // editing a task 
 function editTask(taskItem, taskText) {
@@ -105,53 +91,76 @@ function editTask(taskItem, taskText) {
     
 }
 
-    //background image
-document.addEventListener("DOMContentLoaded", function () {
-    const body = document.body;
-    const imagePath = "./Assets/images/background.jpg";
-    body.style.backgroundImage = `url('${imagePath}')`;
-});
+    //moving completed task to separate list
+function moveCompletedTask(taskItem, taskText, taskDate, taskPriority) {
+    const completedTaskItem = document.createElement('li');
+    completedTaskItem.classList.add("task-item", "completed-task-item");
+    completedTaskItem.innerHTML = `
+        <div class="task-item-container">
+            <span class="task-item task-name">${taskText}</span>
+            <span class="task-item task-date">${taskDate}</span>
+            <span class="task-item task-priority">${taskPriority}</span>
+            <div class="button-container">
+                <button class="restore-button"><i class="fas fa-undo" style="color: #b19cd8;"></i></button>
+            </div>
+        </div>
+    `;
+    completedTaskContainer.appendChild(completedTaskItem);
+    taskItem.remove();
+}
+
 
 completedTaskContainer.addEventListener("click", function (e) {
     const target = e.target;
     if (target.classList.contains("restore-button")) {
-        // Restore completed task
+        // restore completed task
         const completedTaskItem = target.closest(".completed-task-item");
         const taskText = completedTaskItem.querySelector(".task-name").innerText;
-        restoreCompletedTask(completedTaskItem, taskText);
+        const taskDate = completedTaskItem.querySelector(".task-date").innerText;
+        const taskPriority = completedTaskItem.querySelector(".task-priority").innerText;
+
+        restoreCompletedTask(completedTaskItem, taskText, taskDate, taskPriority);
     }
 });
 
 
-function restoreCompletedTask(completedTaskItem, taskText) {
+//restoring function
+function restoreCompletedTask(completedTaskItem, taskText, taskDate, taskPriority) {
     const taskItem = document.createElement('li');
     taskItem.classList.add("task-item");
     taskItem.innerHTML = `
         <div class="task-item-container">
             <span class="task-item task-name">${taskText}</span>
-            <span class="task-item task-date">Completed</span>
-            <div class="button-container">
-                <button class="edit-button"><i class="fas fa-edit" style="color: #b19cd8;"></i></button>
-                <button class="delete-button"><i class="fas fa-trash-alt" style="color: #b19cd8;"></i></button>
-            </div>
+            <span class="task-item task-date">${taskDate}</span>
+            <span class="task-item task-priority">${taskPriority}</span>
+            <button class="button-container edit-button"><i class="fas fa-edit" style="color: #b19cd8;"></i></button>
+            <button class="button-container completed-button"><i class="fas fa-tasks" style="color: #b19cd9;"></i></button>
+            <button class="button-container delete-button"><i class="fas fa-trash-alt" style="color: #b19cd8;"></i></button>
         </div>
     `;
     taskList.appendChild(taskItem);
     completedTaskItem.remove();
 }
 
-const toggleButton = document.getElementById("toggleCompletedTasks");
 toggleButton.addEventListener("click", function () {
     const taskListContainer = document.querySelector(".task-list-container");
     const completedTasksContainer = document.querySelector(".completed-tasks-container");
 
     if (taskListContainer.style.display === "none") {
-        // Show task list and hide completed tasks
+        // hide completed list and show task list
         taskListContainer.style.display = "block";
         completedTasksContainer.style.display = "none";
     } else {
-        // Hide task list and show completed tasks
+        // hide task list and show completed tasks
         taskListContainer.style.display = "none";
         completedTasksContainer.style.display = "block";
     }
+});
+
+
+//background image
+document.addEventListener("DOMContentLoaded", function () {
+    const body = document.body;
+    const imagePath = "./Assets/images/background.jpg";
+    body.style.backgroundImage = `url('${imagePath}')`;
 });
